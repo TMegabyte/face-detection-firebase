1ff7982807b3aa2d2ea5da61166a09250a6b0db9
Ohm Piromrak <ohmpzzz@gmail.com>
2018-12-21 01:37:59 +0700

    Face-Detection

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDSm1ZJg3jrdamchCMD6EkPu7QwHbip00Y',
+    authDomain: 'face-detection-d567f.firebaseapp.com',
+    databaseURL: 'https://face-detection-d567f.firebaseio.com',
+    projectId: 'face-detection-d567f',
+    storageBucket: 'face-detection-d567f.appspot.com',
+    messagingSenderId: '598258340256',
+  },
+};
